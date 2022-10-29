60bf255f22f4708a90860cc8d9fffa1ec468a0dc
MacBook Pro <macbookpro@MacBook-Pro-Macbook.local>
2022-10-29 11:55:55 -0700

    fixed bugs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import styles from "./Header.module.scss"
 
 function Header() {
+  const [activeMenu, setActiveMenu] = useState(false)
     return (
       <div className={styles.header__container}>
         <div className={styles.header__logo}>
             <p>Crypto</p>
             <span className={styles.ellipse}></span>
         </div>
-        <div className={styles.header__menu}>
+        <div className={activeMenu ? styles.header__menu__active : styles.header__menu}>
             <ul>
             <li>Coins</li>
             <li>Featured</li>
             <li>Contact</li>
             <li>About</li>
             </ul>
+        </div>
+        <div className={styles.header__hamburger} onClick={() => setActiveMenu(!activeMenu)}>
+          <span className={styles.header__hamburger__bar}></span>
+          <span className={styles.header__hamburger__bar}></span>
+          <span className={styles.header__hamburger__bar}></span>
         </div>
       </div>
     );
